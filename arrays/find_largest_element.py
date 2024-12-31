4e45bd3829257cc11c54d982977bc2b8e2d8eed1
Amalaasish <139550073+AMALVAASISH@users.noreply.github.com>
2024-12-31 11:53:49 +0530

    Adding files of day 1

---
+++ arrays/find_largest_element.py
@@ -0,0 +1,41 @@
+ # first method is to sort the array
+
+# from typing import List
+#
+# def sortArr(arr: List[int]) -> int:
+#     arr.sort()
+#     return arr[-1]
+#
+#
+# arr2 = [4,17,5,8]
+#
+# print("The largest element is ", sortArr(arr2))
+#
+
+
+# this is the brute force approach
+# Time Complexity: O(N*log(N))
+#
+# Space Complexity: O(n)
+
+
+# second approach is using a max variable
+
+# def findtheLargestnumber(arr, n):
+#     max = arr[0]
+#
+#     for i in range(0,n):
+#         if (max < arr[i]):
+#             max = arr[i]
+#
+#     return max
+#
+# arr1 = [4,0,9,2,4,17,8]
+# n = 7
+#
+# max = findtheLargestnumber(arr1,n)
+# print("this is the largest element",max)
+#
+# Time Complexity: O(N)
+#
+# Space Complexity: O(1)
---
+++ arrays/find_the_second_largest_smallest_in_array.py
@@ -0,0 +1,25 @@
+# brute approach
+
+# def findtheElements(arr,n):
+#     if n == 0 or n == 1:
+#         print(-1,-1)
+#     arr.sort()
+#     small = arr[1]
+#     large = arr[n-2]
+#
+#     print("The second smallest", small)
+#     print("The second largest", large)
+#
+# arr = [4,2,7,3,9,17,8,2]
+# n = 8
+# findtheElements(arr,n)
+#
+#
+# Time Complexity: O(NlogN), For sorting the array
+#
+# Space Complexity: O(1)
+
+
+
+# Better approach
+
